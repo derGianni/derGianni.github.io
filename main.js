50923d6b64b92eff3333560fe0f6dc845d9f34ac
derGianni <98225771+derGianni@users.noreply.github.com>
2022-03-19 22:45:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/*Animation des Navigationsfensters*/
+const menue_btn = document.querySelector('.button')
+const nav = document.querySelector('.nav')
+menue_btn.addEventListener('click', function() {
+    nav.classList.toggle('is-active');
+    menue_btn.classList.toggle('is-active');
+    alphaP
+});
